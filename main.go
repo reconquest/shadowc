891709ee196019982eda60b480b82626e3e03777
Egor Kovetskiy <e.kovetskiy@office.ngs.ru>
2015-02-22 23:19:05 +0300

    get shadow entry skeleton

--- main.go
+++ main.go
@@ -1,24 +1,65 @@
 package main
 
 import (
-	"fmt"
-	"os"
-	"os/user"
+	"log"
+
+	"github.com/docopt/docopt-go"
 )
 
-func init() {
-	user, err := user.Current()
-	if err != nil {
-		fmt.Println(err.Error())
-		os.Exit(1)
+type (
+	ShadowEntry struct {
+		username string
+		hash     string
+		//another fields?
 	}
-
-	if user.Gid != "0" {
-		fmt.Println("User gid must be 0")
-		os.Exit(1)
-	}
-}
+	ShadowEntries []*ShadowEntry
+)
 
 func main() {
+	args, err := getArgs()
+	if err != nil {
+		log.Fatal(err.Error())
+	}
 
+	//user, err := user.Current()
+	//if err != nil {
+	//log.Fatal(err.Error())
+	//}
+
+	//if user.Gid != "0" {
+	//log.Fatal("User gid must be 0")
+	//}
+
+	entries := ShadowEntries{}
+	for _, username := range args["-u"].([]string) {
+		entry, err := getShadowEntry(username)
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+
+		entries = append(entries, entry)
+	}
+
+	log.Printf("%#v", entries)
+}
+
+func getArgs() (map[string]interface{}, error) {
+	usage := `shadowc 0.1
+
+Usage:
+	shadowc [-u <username>...]
+
+Options:
+    -u <username>    Request shadow entry for this username. [default: root]`
+
+	return docopt.Parse(usage, nil, true, "shadowc 0.1", false)
+}
+
+func getShadowEntry(username string) (*ShadowEntry, error) {
+	entry := &ShadowEntry{
+		username: username,
+		hash:     "$1$blah$blah",
+	}
+
+	return entry, nil
 }

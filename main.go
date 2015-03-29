eab47a2e143065d5727932c3a7e47db24f8af513
Egor Kovetskiy <e.kovetskiy@office.ngs.ru>
2015-03-29 17:25:45 +0300

    first work version

--- main.go
+++ main.go
@@ -1,81 +1,71 @@
 package main
 
 import (
-	"bufio"
+	"crypto/tls"
+	"crypto/x509"
+	"encoding/pem"
 	"fmt"
-	"io"
 	"io/ioutil"
 	"log"
+	"net/http"
 	"os"
 	"strings"
 
 	"github.com/docopt/docopt-go"
 )
 
-const usage = `shadowc, login distribution client
+const usage = `shadowc, client of login distribution service.
 
 Usage:
-	shadowc [-u <login>...] -s <repo>... [--print] [--output <file>]
+	shadowc [options] [-u <user>...] -s <addr>...
 
 Options:
-    -u <login>           add login [default: root]
-    -s <repo>            add key repository
-    --stdin              use stdin instead of /etc/shadow
-    --stdout             use stdout instead of /etc/shadow`
+    -u <user>    Specify a user which needs shadow entry [default: root]
+    -s <addr>    Specify a key repository address.
+	-f <file>    Specify a shadow file path [default: /etc/shadow].
+	-c <cert>    Specify a certificate file path [default: /var/shadowd/cert/cert.pem].
+`
 
 func main() {
 	args, _ := docopt.Parse(usage, nil, true, "shadowc 1.0", false)
 
-	repos := args["-s"].([]string)
-	logins := args["-u"].([]string)
+	var (
+		addrs               = args["-s"].([]string)
+		users               = args["-u"].([]string)
+		shadowFilepath      = args["-f"].(string)
+		certificateFilepath = args["-c"].(string)
+	)
 
-	shadows, err := getShadows(logins, repos)
+	shadows, err := getShadows(users, addrs, certificateFilepath)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	writeShadows(shadows, args["--stdin"].(bool), args["--stdout"].(bool))
+	err = writeShadows(shadows, shadowFilepath)
+	if err != nil {
+		panic(err)
+	}
 }
 
-func writeShadows(
-	shadows *Shadows, useStdin bool, useStdout bool,
-) (err error) {
-	var (
-		input    io.Reader
-		output   io.Writer
-		shadowFd io.ReadWriteCloser
-	)
-
-	if !useStdin || !useStdout {
-		shadowFd, err = os.Open("/etc/shadow")
-		if err != nil {
-			return err
-		}
-		defer shadowFd.Close()
-	}
-
-	switch {
-	case useStdin:
-		input = os.Stdin
-	case !useStdin:
-		input = shadowFd
-	case useStdout:
-		input = os.Stdout
-	case !useStdout:
-		output = shadowFd
-	}
-
-	content, err := ioutil.ReadAll(input)
+func writeShadows(shadows *Shadows, shadowFilepath string) (err error) {
+	file, err := os.OpenFile(shadowFilepath, os.O_RDWR, 0600)
 	if err != nil {
 		return err
 	}
 
-	lines := strings.Split(string(content), "\n")
+	defer file.Close()
+
+	content, err := ioutil.ReadAll(file)
+	if err != nil {
+		return err
+	}
+
+	lines := strings.Split(strings.TrimRight(string(content), "\n"), "\n")
 
 	for _, shadow := range *shadows {
 		found := false
 		for lineIndex, line := range lines {
-			if strings.HasPrefix(line, shadow.Login+":") {
+			if strings.HasPrefix(line, shadow.User+":") {
 				lines[lineIndex] = fmt.Sprintf("%s", shadow)
 				found = true
 				break
@@ -87,26 +77,48 @@ func writeShadows(
 		}
 	}
 
-	writer := bufio.NewWriter(output)
-	if _, err = writer.WriteString(strings.Join(lines, "\n")); err != nil {
-		return err
-	}
+	_, err = file.WriteString(strings.Join(lines, "\n") + "\n")
 
-	return writer.Flush()
+	return err
 }
 
-func getShadows(logins, repoAddrs []string) (*Shadows, error) {
-	var err error
+func getShadows(
+	users, addrs []string, certificateFilepath string,
+) (*Shadows, error) {
+	pemData, err := ioutil.ReadFile(certificateFilepath)
+	if err != nil {
+		return nil, err
+	}
 
-	entries := new(Shadows)
-	for _, repoAddr := range repoAddrs {
-		repo, _ := NewKeyRepository(repoAddr)
+	pemBlock, _ := pem.Decode(pemData)
+	if pemBlock == nil {
+		return nil, fmt.Errorf("no PEM data is found in file")
+	}
 
-		entries, err = repo.GetShadows(logins)
+	certificate, err := x509.ParseCertificate(pemBlock.Bytes)
+	if err != nil {
+		return nil, err
+	}
+
+	certPool := x509.NewCertPool()
+	certPool.AddCert(certificate)
+
+	resource := &http.Client{
+		Transport: &http.Transport{
+			TLSClientConfig: &tls.Config{
+				RootCAs: certPool,
+			},
+		},
+	}
+
+	for _, addr := range addrs {
+		repo, _ := NewKeyRepository(addr, resource)
+
+		shadows, err := repo.GetShadows(users)
 		if err == nil {
-			return entries, nil
+			return shadows, nil
 		} else {
-			log.Printf("%#v", err)
+			log.Printf("%s", err)
 
 			// try with next repo
 			continue

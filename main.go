52e54f3e050eda7e52cdd42719772a24bf1f8725
Egor Kovetskiy <e.kovetskiy@office.ngs.ru>
2015-10-10 17:43:32 +0600

    allow request SSH keys even if no shadow entries found

--- main.go
+++ main.go
@@ -188,7 +188,7 @@ func main() {
 		fmt.Println("Shadow information updated")
 	}
 
-	if canUpdateSSHKeys && len(*shadows) > 0 {
+	if canUpdateSSHKeys {
 		fmt.Printf("Updating %d SSH keys...\n", len(authorizedKeys))
 
 		addedKeysTotal := 0
